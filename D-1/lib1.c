49924106700ff75022ba4f4f3184fd10ab463f1d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2013-11-01 12:18:06 +0900

    D-1(m)
    v-1.0.2
    ---
    1. function
    	=> putchar()

---
+++ D-1/lib1.c
@@ -0,0 +1,27 @@
+/*
+
+* lib1.c
+ *
+ *  Created on: 2013/11/01
+ *      Author: IwabuchiK
+ */
+
+#ifndef STDIO_H
+#define STDIO_H
+#include <stdio.h>
+#endif
+
+#ifndef STRING_H
+#define STRING_H
+#include <string.h>
+#endif
+
+//#ifndef D_1_H
+//#define D_1_H
+#include "d_1.h"
+//#endif
+
+void show_shift_jis()
+{
+	printf("shift_jis\n");
+}
